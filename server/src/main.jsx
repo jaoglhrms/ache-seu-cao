bed37c4813e37e843f4837fef9823d502ca4318b
Joao Guilherme <jgnoobgf@gmail.com>
2026-01-15 03:05:53 -0300

    Correção

--- server/src/main.jsx
+++ server/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // COLE SEU ID DO GOOGLE AQUI EMBAIXO ENTRE AS ASPAS
-const clientId = 82709820990-k9lqi3iokjge6p2jqufqq7asajm6g6gc.apps.googleusercontent.com;
+const clientId = "82709820990-k9lqi3iokjge6p2jqufqq7asajm6g6gc.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
